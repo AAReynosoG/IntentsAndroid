6de5c8dc1fcf59f1cbb69264e6bc69cb4ca7cc34
Alonso <abrahamreynoso64@gmail.com>
2023-09-28 08:13:48 -0600

    Cambios

--- app/src/main/java/com/example/intents/MainActivity.java
+++ app/src/main/java/com/example/intents/MainActivity.java
@@ -7,26 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.a8botones.MainActivity2;
-import com.example.a8botones.R;
+import com.example.intents.MainActivity2;
+import com.example.intents.R;
 import com.example.intents.MainActivity2;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button B, B2, B3, B4, B5, B6, B7, B8;
+    Button B, B2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         B = findViewById(R.id.b1);
-        B2 = findViewById(R.id.b2);
-        B3 = findViewById(R.id.b3);
-        B4 = findViewById(R.id.b4);
-
         B.setOnClickListener(this);
-    }
 
+        B2 = findViewById(R.id.b2);
+        B2.setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View view) {
--- app/src/main/java/com/example/intents/MainActivity2.java
+++ app/src/main/java/com/example/intents/MainActivity2.java
@@ -2,13 +2,33 @@ package com.example.intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button B1E, B2E;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        B1E = findViewById(R.id.be1);
+        B2E = findViewById(R.id.be2);
+
+        B1E.setOnClickListener(this);
+        B2E.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        Intent i;
+        Intent i2;
+
     }
 }
